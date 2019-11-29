0992c69ea475564e0d93705b2d7a353adf291219
nitingeo <nitin@geospark.co>
2019-11-29 15:22:05 +0530

    F

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,3 +136,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
