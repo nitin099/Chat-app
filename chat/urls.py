f67533565aabc53938dc1771b29c63dc2bcb7182
nitingeo <nitin@geospark.co>
2019-11-29 15:16:53 +0530

    sadf

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
